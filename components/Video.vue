5c3bbf6b59c8ecfbde463a192deb836b769e69f9
Max Globa <onyx18121990@gmail.com>
2023-09-06 09:36:17 +0300

    Changed thumbs

---
+++ components/Video.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="video">
+    <button class="video-close" @click="videoClose">+</button>
+    <div class="video-container">
+      <iframe v-if="name === 'terry'" src="https://player.vimeo.com/video/860816372?h=d89b70a675&amp;title=0&amp;byline=0&amp;portrait=0&amp;speed=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" width="1920" height="1080" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen title="ERC - video.mp4"></iframe>
+      <iframe v-if="name === 'robert'" src="https://player.vimeo.com/video/860816504?h=634c82e1d1&amp;title=0&amp;byline=0&amp;portrait=0&amp;speed=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" width="1920" height="1080" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen title="ERC - video.mp4"></iframe>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ['name'],
+  methods: {
+    videoClose(){
+      this.$parent.video = false
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.video{
+  position: fixed;
+  left: 0; top: 0;
+  width: 100%;
+  height: 100vh;
+  padding: 30px;
+  z-index: 999;
+  background: rgba(0,0,0, .85);
+  &-container{
+    position: relative;
+    overflow: hidden;
+    width: 100%;
+    height: auto;
+    display: block;
+    padding-top: 56.25%;
+    margin: 0 auto;
+    @media(min-width:768px){
+      width: 700px;
+    }
+    @media(min-width:1200px){
+      width: 1200px;
+    }
+  }
+  iframe{
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+  }
+  &-close{
+    position: absolute;
+    right: 10px;
+    top: 10px;
+    transform: rotate(45deg);
+    background: none;
+    border: none;
+    color: #fff;
+    font-size: 20px;
+    @media(min-width:768px){
+      font-size: 30px;
+    }
+    @media(min-width:1200px){
+      font-size: 60px;
+    }
+  }
+}
+</style>
